380f2da63cd3010e4cfe4efab78b8c967a3384d6
jesben <jesben.sevillano.h@uni.pe>
2020-12-09 16:57:49 -0500

    mongo driver and test

--- api/now.rb
+++ api/now.rb
@@ -1,5 +1,9 @@
+client = Mongo::Client.new(ENV["MONGO_URL"])
+
 Handler = Proc.new do |req, res|
+  document = { name: "Harriet", age: 36 }
+  result = client[:artists].insert_one( document )
   res.status = 200
   res['Content-Type'] = 'text/text; charset=utf-8'
-  res.body = "Hoy es: #{Time.new}"
+  res.body = "Pude insertar #{ result.n } documentos"
 end
